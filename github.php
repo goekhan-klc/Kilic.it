c91ceb0a3d574962fee84172f19d7c8fbceb1ef7
Gökhan <goekhan.x@icloud.com>
2024-03-12 21:16:06 +0100

    Delete github.php

--- github.php
+++
@@ -1,6 +0,0 @@
-<?php 
-exec("git pull https://goekhan.klc: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@github.com/goekhan-klc/space4_kilicit master", $out);
-foreach ($out as $line) {
-    echo $line . "\n";
-}
-?>
