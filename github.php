6c451f55332ea22c5e9422c018db0d7e58d349a4
Gökhan <goekhan.x@icloud.com>
2024-03-12 21:15:14 +0100

    Workflow update

--- github.php
+++ github.php
@@ -1,5 +1,5 @@
 <?php 
-exec("git pull", $out);
+exec("git pull https://goekhan.klc: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@github.com/goekhan-klc/space4_kilicit master", $out);
 foreach ($out as $line) {
     echo $line . "\n";
 }
